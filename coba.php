00684e31b292cb28ca82ede4097f7917c1deac28
ariasrupawansyah <193040140.aria@mail.unpas.ac.id>
2021-09-17 16:11:19 +0700

    Mempelajari materi beserta pembuatan class dan object pada php (video materi ke 2 oophp)

---
+++ coba.php
@@ -0,0 +1,17 @@
+<?php
+
+// Aria Rupawansyah - 193040140
+
+// class Coba
+class Coba{
+
+}
+
+// instansiasi object
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
