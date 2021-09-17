fd0a4f5ab131154b28330a37fc7b108358619c8a
ariasrupawansyah <193040140.aria@mail.unpas.ac.id>
2021-09-17 16:13:02 +0700

    Mempelajari materi beserta pembuatan class dan object pada php (video materi ke 2 oophp)

---
+++ produk.php
@@ -0,0 +1,16 @@
+<?php
+
+// Aria Rupawansyah - 193040140
+
+// Jualan Produk
+// Komik
+// Game
+
+// class produk
+class Produk{
+    
+}
+
+// instansiasi objek dari class produk
+$produk1 = new Produk();
+$produk2 = new Produk();
