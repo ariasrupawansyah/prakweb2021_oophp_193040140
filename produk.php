973230d1a2e38712c140809dc2b1c11fe5d77394
ariasrupawansyah <193040140.aria@mail.unpas.ac.id>
2021-09-17 16:18:16 +0700

    Mempelajari materi beserta pembuatan property dan method pada php dengan memperbaharui file produk.php (video materi ke 3 oophp)

--- produk.php
+++ produk.php
@@ -8,9 +8,41 @@
 
 // class produk
 class Produk{
-    
+
+    public $judul = "judul", 
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0; 
+
+
+    // method untuk menampilkan penulis dan penerbit ke layar
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
 }
 
+
 // instansiasi objek dari class produk
-$produk1 = new Produk();
-$produk2 = new Produk();
+$produk3 = new Produk();
+// properties
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+
+// instansiasi objek dari class produk
+$produk4 = new Produk();
+// properties
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+// menampilkan objek ke layar
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
+
+?>
